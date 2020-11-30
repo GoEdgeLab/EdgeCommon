322d9af8c61d90cc71c71125c9de88d506d755bb
goedgecloud <goedgecloud@gmail.com>
2020-11-30 20:01:27 +0800

    源站可以设置名称、描述、是否启用

--- pkg/serverconfigs/filterconfigs/filter_unicode_decode_test.go
+++ pkg/serverconfigs/filterconfigs/filter_unicode_decode_test.go
@@ -7,4 +7,9 @@ func TestUnicodeDecodeFilter_Do(t *testing.T) {
 	t.Log(filter.Do(`"\u5947\u5c4f`, nil))
 	t.Log(filter.Do(`"Hello`, nil))
 	t.Log(filter.Do(`真实的存在`, nil))
+
+	t.Logf("%x", "/dashboard")
+	t.Log(filter.Do("\x2f\x64\x61\x73\x68\x62\x6f\x61\x72\x64", nil))
 }
+
+
