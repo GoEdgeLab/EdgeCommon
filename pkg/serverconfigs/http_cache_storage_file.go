3d50305c567d400a8b4f89988356673e49bf34af
goedgecloud <goedgecloud@gmail.com>
2021-03-16 18:09:29 +0800

    提交二级缓存缺失代码

--- pkg/serverconfigs/http_cache_storage_file.go
+++ pkg/serverconfigs/http_cache_storage_file.go
@@ -1,7 +1,9 @@
 package serverconfigs
 
+// 文件缓存存储策略
 type HTTPFileCacheStorage struct {
-	Dir string `yaml:"dir" json:"dir"` // 目录
+	Dir          string           `yaml:"dir" json:"dir"`                   // 目录
+	MemoryPolicy *HTTPCachePolicy `yaml:"memoryPolicy" json:"memoryPolicy"` // 内存二级缓存
 }
 
 func (this *HTTPFileCacheStorage) Init() error {
