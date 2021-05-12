198fcdcd6dd2c602b00b45e0dec27ad4cd51c415
goedgecloud <goedgecloud@gmail.com>
2021-05-12 16:10:17 +0800

    增加是否自动添加X-Cache选项

--- pkg/serverconfigs/http_cache_config.go
+++ pkg/serverconfigs/http_cache_config.go
@@ -6,6 +6,8 @@ type HTTPCacheConfig struct {
 	IsPrior bool `yaml:"isPrior" json:"isPrior"`
 	IsOn    bool `yaml:"isOn" json:"isOn"`
 
+	AddStatusHeader bool `yaml:"addStatusHeader" json:"addStatusHeader"` // 是否增加命中状态Header
+
 	CacheRefs []*HTTPCacheRef `yaml:"cacheRefs" json:"cacheRefs"` // 缓存配置
 }
 
