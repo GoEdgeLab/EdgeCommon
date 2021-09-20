15fa8971914655db67cfd0f50525115081152357
goedgecloud <goedgecloud@gmail.com>
2021-09-20 16:38:00 +0800

    在集群中可以设置自动加入DNS的CNAME记录

--- pkg/dnsconfigs/cluster_dns_config.go
+++ pkg/dnsconfigs/cluster_dns_config.go
@@ -2,6 +2,8 @@ package dnsconfigs
 
 // ClusterDNSConfig 集群的DNS设置
 type ClusterDNSConfig struct {
+	CNameRecords []string `yaml:"cnameRecords" json:"cnameRecords"` // 自动加入的CNAME
+
 	NodesAutoSync   bool `yaml:"nodesAutoSync" json:"nodesAutoSync"`     // 是否自动同步节点状态
 	ServersAutoSync bool `yaml:"serversAutoSync" json:"serversAutoSync"` // 是否自动同步服务状态
 }
