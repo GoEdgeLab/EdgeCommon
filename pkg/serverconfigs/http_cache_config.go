0ec7e3f221c8dab09bc124ab5a8d1cfb10e9627c
goedgecloud <goedgecloud@gmail.com>
2021-10-17 20:22:01 +0800

    增加PURGE某个URL缓存功能

--- pkg/serverconfigs/http_cache_config.go
+++ pkg/serverconfigs/http_cache_config.go
@@ -1,12 +1,17 @@
 package serverconfigs
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"github.com/iwind/TeaGo/rands"
+)
 
 type HTTPCacheConfig struct {
 	IsPrior bool `yaml:"isPrior" json:"isPrior"`
 	IsOn    bool `yaml:"isOn" json:"isOn"`
 
-	AddStatusHeader bool `yaml:"addStatusHeader" json:"addStatusHeader"` // 是否增加命中状态Header
+	AddStatusHeader bool   `yaml:"addStatusHeader" json:"addStatusHeader"` // 是否增加命中状态Header
+	PurgeIsOn       bool   `yaml:"purgeIsOn" json:"purgeIsOn"`             // 是否允许使用Purge方法清理
+	PurgeKey        string `yaml:"purgeKey" json:"purgeKey"`               // Purge时使用的Edge-Purge-Key
 
 	CacheRefs []*HTTPCacheRef `yaml:"cacheRefs" json:"cacheRefs"` // 缓存配置
 }
@@ -18,6 +23,11 @@ func (this *HTTPCacheConfig) Init() error {
 			return err
 		}
 	}
+
+	if this.PurgeIsOn && len(this.PurgeKey) == 0 {
+		this.PurgeKey = rands.HexString(32)
+	}
+
 	return nil
 }
 
