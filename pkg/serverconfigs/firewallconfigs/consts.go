7a5757cae6da5ddc70499f03d2d8864d5da3a595
goedgecloud <goedgecloud@gmail.com>
2021-11-17 16:15:01 +0800

    IP名单增加是否全局

---
+++ pkg/serverconfigs/firewallconfigs/consts.go
@@ -0,0 +1,8 @@
+// Copyright 2021 Liuxiangchao iwind.liu@gmail.com. All rights reserved.
+
+package firewallconfigs
+
+const (
+	GlobalListId      int64 = 2_000_000_000
+	DefaultEventLevel       = "critical"
+)
