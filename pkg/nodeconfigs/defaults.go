cc1314f146779f2886b49306624342181bb9b327
goedgecloud <goedgecloud@gmail.com>
2021-12-22 16:43:26 +0800

    增加常量

--- pkg/nodeconfigs/defaults.go
+++ pkg/nodeconfigs/defaults.go
@@ -9,6 +9,7 @@ const (
 	DefaultMaxThreadsMin = 1000    // 单节点最大线程数最小值
 	DefaultMaxThreadsMax = 100_000 // 单节点最大线程数最大值
 
-	DefaultTCPMaxConnections = 100_000 // 单节点TCP最大连接数
-	DefaultTCPLinger         = 3       // 单节点TCP Linger值
+	DefaultTCPMaxConnections   = 100_000 // 单节点TCP最大连接数
+	DefaultTCPLinger           = 3       // 单节点TCP Linger值
+	DefaultTLSHandshakeTimeout = 3       // TLS握手超时时间
 )
