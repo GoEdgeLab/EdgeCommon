536fe0c8997b770b0670b7ca0c71627c8cbe5f17
goedgecloud <goedgecloud@gmail.com>
2022-02-24 20:13:16 +0800

    增加是否同步写入压缩缓存设置

--- pkg/serverconfigs/http_cache_policy.go
+++ pkg/serverconfigs/http_cache_policy.go
@@ -8,18 +8,19 @@ import (
 
 // HTTPCachePolicy 缓存策略配置
 type HTTPCachePolicy struct {
-	Id          int64                  `yaml:"id" json:"id"`
-	IsOn        bool                   `yaml:"isOn" json:"isOn"`               // 是否开启
-	Name        string                 `yaml:"name" json:"name"`               // 名称
-	Description string                 `yaml:"description" json:"description"` // 描述
-	Capacity    *shared.SizeCapacity   `yaml:"capacity" json:"capacity"`       // 最大内容容量
-	MaxKeys     int64                  `yaml:"maxKeys" json:"maxKeys"`         // 最多Key值
-	MaxSize     *shared.SizeCapacity   `yaml:"maxSize" json:"maxSize"`         // 单个缓存最大尺寸
-	Type        CachePolicyStorageType `yaml:"type" json:"type"`               // 类型
-	Options     map[string]interface{} `yaml:"options" json:"options"`         // 选项
-	Life        *shared.TimeDuration   `yaml:"life" json:"life"`               // 默认有效期 TODO 需要实现
-	MinLife     *shared.TimeDuration   `yaml:"minLife" json:"minLife"`         // 最小有效期 TODO 需要实现
-	MaxLife     *shared.TimeDuration   `yaml:"maxLife" json:"maxLife"`         // 最大有效期 TODO 需要实现
+	Id                   int64                  `yaml:"id" json:"id"`
+	IsOn                 bool                   `yaml:"isOn" json:"isOn"`                                 // 是否开启
+	Name                 string                 `yaml:"name" json:"name"`                                 // 名称
+	Description          string                 `yaml:"description" json:"description"`                   // 描述
+	Capacity             *shared.SizeCapacity   `yaml:"capacity" json:"capacity"`                         // 最大内容容量
+	MaxKeys              int64                  `yaml:"maxKeys" json:"maxKeys"`                           // 最多Key值
+	MaxSize              *shared.SizeCapacity   `yaml:"maxSize" json:"maxSize"`                           // 单个缓存最大尺寸
+	Type                 CachePolicyStorageType `yaml:"type" json:"type"`                                 // 类型
+	Options              map[string]interface{} `yaml:"options" json:"options"`                           // 选项
+	Life                 *shared.TimeDuration   `yaml:"life" json:"life"`                                 // 默认有效期 TODO 需要实现
+	MinLife              *shared.TimeDuration   `yaml:"minLife" json:"minLife"`                           // 最小有效期 TODO 需要实现
+	MaxLife              *shared.TimeDuration   `yaml:"maxLife" json:"maxLife"`                           // 最大有效期 TODO 需要实现
+	SyncCompressionCache bool                   `yaml:"syncCompressionCache" json:"syncCompressionCache"` // 同步写入压缩缓存
 
 	CacheRefs []*HTTPCacheRef `yaml:"cacheRefs" json:"cacheRefs"` // 缓存配置
 
