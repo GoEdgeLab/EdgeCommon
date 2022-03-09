79367221b60486d57fb2e471afe8b29ec9688359
goedgecloud <goedgecloud@gmail.com>
2022-03-09 10:01:29 +0800

    增加对访问日志自动分表配置

--- pkg/serverconfigs/access_log_queue_config.go
+++ pkg/serverconfigs/access_log_queue_config.go
@@ -7,4 +7,7 @@ type AccessLogQueueConfig struct {
 	MaxLength      int `yaml:"maxLength" json:"maxLength"`           // 队列最大长度
 	CountPerSecond int `yaml:"countPerSecond" json:"countPerSecond"` // 每秒写入数量
 	Percent        int `yaml:"percent" json:"percent"`               // 比例，如果为0-100，默认为100
+
+	EnableAutoPartial bool  `yaml:"enableAutoPartial" json:"enableAutoPartial"` // 是否启动自动分表
+	RowsPerTable      int64 `yaml:"rowsPerTable" json:"rowsPerTable"`           // 单表最大行数
 }
