8d12ee72e02d49126607f48d00079339f1a19f1d
goedgecloud <goedgecloud@gmail.com>
2022-04-04 19:45:26 +0800

    增加Sendfile相关选项

--- pkg/serverconfigs/http_cache_storage_file.go
+++ pkg/serverconfigs/http_cache_storage_file.go
@@ -5,7 +5,8 @@ type HTTPFileCacheStorage struct {
 	Dir          string           `yaml:"dir" json:"dir"`                   // 目录
 	MemoryPolicy *HTTPCachePolicy `yaml:"memoryPolicy" json:"memoryPolicy"` // 内存二级缓存
 
-	OpenFileCache *OpenFileCacheConfig `yaml:"openFileCache" json:"openFileCache"` // open file cache配置
+	OpenFileCache  *OpenFileCacheConfig `yaml:"openFileCache" json:"openFileCache"`   // open file cache配置
+	EnableSendfile bool                 `yaml:"enableSendFile" json:"enableSendfile"` // 是否启用Sendfile
 }
 
 func (this *HTTPFileCacheStorage) Init() error {
