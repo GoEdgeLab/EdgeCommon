20dadf2e4431e3f5b5ad4aa6fdb505999365a239
goedgecloud <goedgecloud@gmail.com>
2022-06-25 19:22:19 +0800

    增加修改服务所在分组API/proto文件增加若干注释

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+echo "starting ..."
+
 #rm -f ../pkg/rpc/pb/*.pb.go
+
 protoc --go_out=plugins=grpc:../pkg/rpc --proto_path=../pkg/rpc/protos ../pkg/rpc/protos/*.proto
 RESULT=$?
 if [ "${RESULT}" != "0" ]; then
