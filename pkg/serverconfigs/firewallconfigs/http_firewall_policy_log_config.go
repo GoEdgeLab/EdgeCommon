b25c8699665e060f33976f21aad63f1f49fa5c04
goedgecloud <goedgecloud@gmail.com>
2022-07-16 18:45:50 +0800

    WAF策略增加记录区域封禁日志选项

--- pkg/serverconfigs/firewallconfigs/http_firewall_policy_log_config.go
+++ pkg/serverconfigs/firewallconfigs/http_firewall_policy_log_config.go
@@ -3,14 +3,16 @@
 package firewallconfigs
 
 var DefaultHTTPFirewallPolicyLogConfig = &HTTPFirewallPolicyLogConfig{
-	IsOn:        true,
-	RequestBody: true,
+	IsOn:          true,
+	RequestBody:   true,
+	RegionDenying: false,
 }
 
 type HTTPFirewallPolicyLogConfig struct {
-	IsPrior     bool `yaml:"isPrior" json:"isPrior"`
-	IsOn        bool `yaml:"isOn" json:"isOn"`
-	RequestBody bool `yaml:"requestBody" json:"requestBody"`
+	IsPrior       bool `yaml:"isPrior" json:"isPrior"`
+	IsOn          bool `yaml:"isOn" json:"isOn"`
+	RequestBody   bool `yaml:"requestBody" json:"requestBody"`     // 是否记录RequestBody
+	RegionDenying bool `yaml:"regionDenying" json:"regionDenying"` // 是否记录区域封禁日志
 }
 
 func (this *HTTPFirewallPolicyLogConfig) Init() error {
