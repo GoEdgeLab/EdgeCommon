0f79c989daeb05e75def8868949183d95eef24ea
goedgecloud <goedgecloud@gmail.com>
2022-08-09 18:12:15 +0800

    访问日志ES存储增加Data Stream模式

--- pkg/serverconfigs/access_log_storage_es.go
+++ pkg/serverconfigs/access_log_storage_es.go
@@ -4,9 +4,10 @@ package serverconfigs
 
 // AccessLogESStorageConfig ElasticSearch存储策略
 type AccessLogESStorageConfig struct {
-	Endpoint    string `yaml:"endpoint" json:"endpoint"`
-	Index       string `yaml:"index" json:"index"`
-	MappingType string `yaml:"mappingType" json:"mappingType"`
-	Username    string `yaml:"username" json:"username"`
-	Password    string `yaml:"password" json:"password"`
+	Endpoint     string `yaml:"endpoint" json:"endpoint"`
+	Index        string `yaml:"index" json:"index"`
+	MappingType  string `yaml:"mappingType" json:"mappingType"`
+	Username     string `yaml:"username" json:"username"`
+	Password     string `yaml:"password" json:"password"`
+	IsDataStream bool   `yaml:"isDataStream" json:"isDataStream"` // 是否为Data Stream模式
 }
