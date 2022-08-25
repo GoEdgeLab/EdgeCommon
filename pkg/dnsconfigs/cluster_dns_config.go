2822a9c3caaa0af669e61a52a650a256ae6aad69
goedgecloud <goedgecloud@gmail.com>
2022-08-25 19:18:25 +0800

    集群DNS设置中增加”包含Ln节点“选项

--- pkg/dnsconfigs/cluster_dns_config.go
+++ pkg/dnsconfigs/cluster_dns_config.go
@@ -2,9 +2,10 @@ package dnsconfigs
 
 // ClusterDNSConfig 集群的DNS设置
 type ClusterDNSConfig struct {
-	CNameRecords  []string `yaml:"cnameRecords" json:"cnameRecords"`   // 自动加入的CNAME
-	TTL           int32    `yaml:"ttl" json:"ttl"`                     // 默认TTL，各个DNS服务商对记录的TTL的限制各有不同
-	CNameAsDomain bool     `yaml:"cnameAsDomain" json:"cnameAsDomain"` // 是否可以像域名一样直接访问CNAME
+	CNameRecords     []string `yaml:"cnameRecords" json:"cnameRecords"`         // 自动加入的CNAME
+	TTL              int32    `yaml:"ttl" json:"ttl"`                           // 默认TTL，各个DNS服务商对记录的TTL的限制各有不同
+	CNameAsDomain    bool     `yaml:"cnameAsDomain" json:"cnameAsDomain"`       // 是否可以像域名一样直接访问CNAME
+	IncludingLnNodes bool     `yaml:"includingLnNodes" json:"includingLnNodes"` // 是否包含Ln节点
 
 	NodesAutoSync   bool `yaml:"nodesAutoSync" json:"nodesAutoSync"`     // 是否自动同步节点状态
 	ServersAutoSync bool `yaml:"serversAutoSync" json:"serversAutoSync"` // 是否自动同步服务状态
