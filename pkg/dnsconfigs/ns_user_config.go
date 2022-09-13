181c733d21ca31f22ad71caf4a765d7b08ef963e
goedgecloud <goedgecloud@gmail.com>
2022-09-13 10:50:26 +0800

    增加DNS套餐相关API

--- pkg/dnsconfigs/ns_user_config.go
+++ pkg/dnsconfigs/ns_user_config.go
@@ -4,10 +4,28 @@ package dnsconfigs
 
 func DefaultNSUserConfig() *NSUserConfig {
 	return &NSUserConfig{
-		DefaultClusterId: 0,
+		DefaultClusterId:  0,
+		DefaultPlanConfig: DefaultNSUserPlanConfig(),
+	}
+}
+
+func DefaultNSUserPlanConfig() *NSPlanConfig {
+	return &NSPlanConfig{
+		SupportCountryRoutes:           true,
+		SupportChinaProvinceRoutes:     true,
+		SupportISPRoutes:               true,
+		MaxCustomRoutes:                0,
+		MaxLoadBalanceRecordsPerRecord: 100,
+		MinTTL:                         60,
+		MaxDomains:                     100,
+		MaxRecordsPerDomain:            1000,
+		SupportRecordStats:             true,
+		SupportDomainAlias:             false,
+		SupportAPI:                     false,
 	}
 }
 
 type NSUserConfig struct {
-	DefaultClusterId int64 `json:"defaultClusterId"` // 默认部署到的集群
+	DefaultClusterId  int64         `json:"defaultClusterId"`  // 默认部署到的集群
+	DefaultPlanConfig *NSPlanConfig `json:"defaultPlanConfig"` // 默认套餐设置
 }
