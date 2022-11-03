b0653706c1fcb45164596c30a93942ff8e9a0288
goedgecloud <goedgecloud@gmail.com>
2022-11-03 14:58:25 +0800

    时钟同步增加是否检查chrony选项

--- pkg/nodeconfigs/clock_config.go
+++ pkg/nodeconfigs/clock_config.go
@@ -4,15 +4,17 @@ package nodeconfigs
 
 func DefaultClockConfig() *ClockConfig {
 	return &ClockConfig{
-		AutoSync: true,
-		Server:   "",
+		AutoSync:    true,
+		Server:      "",
+		CheckChrony: true,
 	}
 }
 
 // ClockConfig 时钟相关配置
 type ClockConfig struct {
-	AutoSync bool   `yaml:"autoSync" json:"autoSync"` // 自动尝试同步时钟
-	Server   string `yaml:"server" json:"server"`     // 时钟同步服务器
+	AutoSync    bool   `yaml:"autoSync" json:"autoSync"`       // 自动尝试同步时钟
+	Server      string `yaml:"server" json:"server"`           // 时钟同步服务器
+	CheckChrony bool   `yaml:"checkChrony" json:"checkChrony"` // 检查 chronyd 是否在运行
 }
 
 func (this *ClockConfig) Init() error {
