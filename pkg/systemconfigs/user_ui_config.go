1e6a11ae39afb2bbc8e9a757fed5862249b7717f
goedgecloud <goedgecloud@gmail.com>
2022-12-28 16:36:37 +0800

    用户系统增加自定义页脚

--- pkg/systemconfigs/user_ui_config.go
+++ pkg/systemconfigs/user_ui_config.go
@@ -8,15 +8,16 @@ import (
 
 // UserUIConfig 用户界面相关配置
 type UserUIConfig struct {
-	ProductName        string `json:"productName"`        // 产品名
-	UserSystemName     string `json:"userSystemName"`     // 管理员系统名称
-	ShowOpenSourceInfo bool   `json:"showOpenSourceInfo"` // 是否显示开源信息
-	ShowVersion        bool   `json:"showVersion"`        // 是否显示版本号
-	Version            string `json:"version"`            // 显示的版本号
-	ShowFinance        bool   `json:"showFinance"`        // 是否显示财务相关信息
-	FaviconFileId      int64  `json:"faviconFileId"`      // Favicon文件ID
-	LogoFileId         int64  `json:"logoFileId"`         // Logo文件ID
-	TimeZone           string `json:"timeZone"`           // 时区
+	ProductName    string `json:"productName"`    // 产品名
+	UserSystemName string `json:"userSystemName"` // 管理员系统名称
+	ShowPageFooter bool   `json:"showPageFooter"` // 是否显示页脚
+	PageFooterHTML string `json:"pageFooterHTML"` // 页脚HTML
+	ShowVersion    bool   `json:"showVersion"`    // 是否显示版本号
+	Version        string `json:"version"`        // 显示的版本号
+	ShowFinance    bool   `json:"showFinance"`    // 是否显示财务相关信息
+	FaviconFileId  int64  `json:"faviconFileId"`  // Favicon文件ID
+	LogoFileId     int64  `json:"logoFileId"`     // Logo文件ID
+	TimeZone       string `json:"timeZone"`       // 时区
 
 	BandwidthUnit       BandwidthUnit `json:"bandwidthUnit"`       // 带宽单位
 	ShowTrafficCharts   bool          `json:"showTrafficCharts"`   // 是否显示流量相关图表和数据
@@ -32,7 +33,7 @@ func DefaultUserUIConfig() *UserUIConfig {
 	var config = &UserUIConfig{
 		ProductName:         "GoEdge",
 		UserSystemName:      "GoEdge用户系统",
-		ShowOpenSourceInfo:  true,
+		ShowPageFooter:      false,
 		ShowVersion:         true,
 		ShowFinance:         true,
 		BandwidthUnit:       BandwidthUnitBit,
