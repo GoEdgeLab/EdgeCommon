321cb6c740f1196d35f04f773d1de1da9d60f797
goedgecloud <goedgecloud@gmail.com>
2023-03-01 18:59:47 +0800

    WAF拦截动作可以设置最大封禁时间，从而实现封禁时间随机

--- pkg/serverconfigs/firewallconfigs/http_firewall_action_block.go
+++ pkg/serverconfigs/firewallconfigs/http_firewall_action_block.go
@@ -9,7 +9,8 @@ type HTTPFirewallBlockAction struct {
 	StatusCode int           `yaml:"statusCode" json:"statusCode"`
 	Body       string        `yaml:"body" json:"body"` // supports HTML
 	URL        string        `yaml:"url" json:"url"`
-	Timeout    int32         `yaml:"timeout" json:"timeout"`
+	Timeout    int32         `yaml:"timeout" json:"timeout"`       // 最小封禁时长
+	TimeoutMax int32         `yaml:"timeoutMax" json:"timeoutMax"` // 最大封禁时长
 	Scope      FirewallScope `yaml:"scope" json:"scope"`
 }
 
