0da9f1863fdb4fc3246797091aaf1d95dea35031
goedgecloud <goedgecloud@gmail.com>
2023-03-01 16:52:30 +0800

    WAF支持忽略全局WAF规则

--- pkg/serverconfigs/firewallconfigs/http_firewall_ref.go
+++ pkg/serverconfigs/firewallconfigs/http_firewall_ref.go
@@ -1,9 +1,10 @@
 package firewallconfigs
 
 type HTTPFirewallRef struct {
-	IsPrior          bool  `yaml:"isPrior" json:"isPrior"`
-	IsOn             bool  `yaml:"isOn" json:"isOn"`
-	FirewallPolicyId int64 `yaml:"firewallPolicyId" json:"firewallPolicyId"`
+	IsPrior           bool  `yaml:"isPrior" json:"isPrior"`
+	IsOn              bool  `yaml:"isOn" json:"isOn"`
+	FirewallPolicyId  int64 `yaml:"firewallPolicyId" json:"firewallPolicyId"`
+	IgnoreGlobalRules bool  `yaml:"ignoreGlobalRules" json:"ignoreGlobalRules"` // 忽略系统定义的全局规则
 }
 
 func (this *HTTPFirewallRef) Init() error {
