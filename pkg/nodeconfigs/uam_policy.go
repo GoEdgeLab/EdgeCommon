62affc6202dc33035571db2bd055727c0070e0b1
goedgecloud <goedgecloud@gmail.com>
2023-03-17 12:15:30 +0800

    删除不必要的文件内容

--- pkg/nodeconfigs/uam_policy.go
+++ pkg/nodeconfigs/uam_policy.go
@@ -1,26 +1,12 @@
-// Copyright 2022 Liuxiangchao iwind.liu@gmail.com. All rights reserved. Official site: https://goedge.cn .
+// Copyright 2023 Liuxiangchao iwind.liu@gmail.com. All rights reserved. Official site: https://goedge.cn .
+//go:build !plus
 
 package nodeconfigs
 
-func init() {
-	_ = DefaultUAMPolicy.Init()
-}
-
-var DefaultUAMPolicy = &UAMPolicy{
-	IsOn:               true,
-	AllowSearchEngines: true,
-	DenySpiders:        true,
-	UITitle:            "",
-	UIBody:             "",
-}
+var DefaultUAMPolicy = &UAMPolicy{}
 
 type UAMPolicy struct {
-	IsOn               bool `yaml:"isOn" json:"isOn"`
-	AllowSearchEngines bool `yaml:"allowSearchEngines" json:"allowSearchEngines"` // 直接跳过常见搜索引擎
-	DenySpiders        bool `yaml:"denySpiders" json:"denySpiders"`               // 拦截常见爬虫
-
-	UITitle string `yaml:"uiTitle" json:"uiTitle"` // 页面标题
-	UIBody  string `yaml:"uiBody" json:"uiBody"`   // 页面内容
+	IsOn bool `yaml:"isOn" json:"isOn"`
 }
 
 func (this *UAMPolicy) Init() error {
