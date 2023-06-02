abffa6dc247325f35e60e26cbd84e9322c50195d
goedgecloud <goedgecloud@gmail.com>
2023-06-02 14:58:08 +0800

    增加一处测试用例

--- pkg/configutils/ip_test.go
+++ pkg/configutils/ip_test.go
@@ -39,3 +39,8 @@ func TestIPVersion(t *testing.T) {
 	a.IsTrue(configutils.IPVersion(net.ParseIP("::1")) == 6)
 	a.IsTrue(configutils.IPVersion(net.ParseIP("2001:0db8:85a3:0000:0000:8a2e:0370:7334")) == 6)
 }
+
+
+func TestQuoteIP(t *testing.T) {
+	t.Log(configutils.QuoteIP(configutils.QuoteIP("2001:da8:22::10")))
+}
