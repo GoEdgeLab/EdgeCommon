068ca49b47fa175fae3d45a51ee8952d999604c8
goedgecloud <goedgecloud@gmail.com>
2023-07-09 16:17:03 +0800

    增加是否在手机浏览器上也支持HTTP/3的选项

--- pkg/nodeconfigs/http3_policy.go
+++ pkg/nodeconfigs/http3_policy.go
@@ -5,13 +5,15 @@ package nodeconfigs
 const DefaultHTTP3Port = 443
 
 type HTTP3Policy struct {
-	IsOn bool `yaml:"isOn" json:"isOn"`
-	Port int  `yaml:"port" json:"port"`
+	IsOn                  bool `yaml:"isOn" json:"isOn"`
+	Port                  int  `yaml:"port" json:"port"`
+	SupportMobileBrowsers bool `yaml:"supportMobileBrowsers" json:"supportMobileBrowsers"` // enable http/3 on common mobile browsers
 }
 
 func NewHTTP3Policy() *HTTP3Policy {
 	return &HTTP3Policy{
-		Port: DefaultHTTP3Port,
+		Port:                  DefaultHTTP3Port,
+		SupportMobileBrowsers: false,
 	}
 }
 
