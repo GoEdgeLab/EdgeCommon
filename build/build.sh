5fe722a27b858e91732b537819d5d1d11c9486dc
goedgecloud <goedgecloud@gmail.com>
2023-08-06 23:36:50 +0800

    使用最新的protoc-gen-go和protoc-gen-go-grpc插件

--- build/build.sh
+++ build/build.sh
@@ -4,13 +4,13 @@ echo "starting ..."
 
 #rm -f ../pkg/rpc/pb/*.pb.go
 
-protoc --go_out=plugins=grpc:../pkg/rpc --proto_path=../pkg/rpc/protos ../pkg/rpc/protos/*.proto
+protoc --go-grpc_out=../pkg/rpc --go-grpc_opt=require_unimplemented_servers=false --proto_path=../pkg/rpc/protos  ../pkg/rpc/protos/*.proto
 RESULT=$?
 if [ "${RESULT}" != "0" ]; then
 	exit
 fi
 
-protoc --go_out=plugins=grpc:../pkg/rpc --proto_path=../pkg/rpc/protos ../pkg/rpc/protos/models/*.proto
+protoc --go-grpc_out=../pkg/rpc --proto_path=../pkg/rpc/protos ../pkg/rpc/protos/models/*.proto
 RESULT=$?
 if [ "${RESULT}" != "0" ]; then
 	exit
