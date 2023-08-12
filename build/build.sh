11d6eccc7abbb898cfd114d1380e7b0857046961
goedgecloud <goedgecloud@gmail.com>
2023-08-12 20:48:53 +0800

    编译proto文件时捕捉错误提示

--- build/build.sh
+++ build/build.sh
@@ -4,6 +4,10 @@ echo "starting ..."
 
 #rm -f ../pkg/rpc/pb/*.pb.go
 protoc --go_out=../pkg/rpc --proto_path=../pkg/rpc/protos  ../pkg/rpc/protos/*.proto
+RESULT=$?
+if [ "${RESULT}" != "0" ]; then
+	exit
+fi
 
 protoc --go-grpc_out=../pkg/rpc --go-grpc_opt=require_unimplemented_servers=false --proto_path=../pkg/rpc/protos  ../pkg/rpc/protos/*.proto
 RESULT=$?
