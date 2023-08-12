53869973949c29f956c43c6c7cee4e0fdbeef6dd
goedgecloud <goedgecloud@gmail.com>
2023-08-12 20:33:28 +0800

    优化API错误相关提示

--- pkg/rpc/errors/utils.go
+++ pkg/rpc/errors/utils.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"path/filepath"
 	"strings"
 )
 
@@ -26,7 +27,7 @@ func HumanError(err error, endpoints []string, configFile string) (resultErr err
 	case codes.Unimplemented:
 		return fmt.Errorf("请求的RPC服务或方法不存在，可能是没有升级API节点或者当前节点没有升级：%w", err), false
 	case codes.Unavailable:
-		return fmt.Errorf("RPC当前不可用：<br/>1、请确认当前节点的api.yaml（<em>%s</em>）配置中的地址（<em>%s</em>）是否已填写正确；<br/>2、请确保API节点已启动，并检查当前节点和API节点之间的网络连接是正常的。<hr/>错误信息：%w", configFile, strings.Join(endpoints, ", "), err), true
+		return fmt.Errorf("RPC当前不可用：<br/>1、请确认当前节点的%s（<em>%s</em>）配置中的地址（<em>%s</em>）是否已填写正确；<br/>2、请确保API节点已启动，并检查当前节点和API节点之间的网络连接是正常的。<hr/>错误信息：%w", filepath.Base(configFile), configFile, strings.Join(endpoints, ", "), err), true
 	}
 
 	return err, false
