3007bac2fb22db4240677939d39e4b86df45092d
goedgecloud <goedgecloud@gmail.com>
2023-09-07 11:46:20 +0800

    套餐增加请求数（日/月）限制

--- pkg/serverconfigs/traffic_limit_status.go
+++ pkg/serverconfigs/traffic_limit_status.go
@@ -4,11 +4,19 @@ package serverconfigs
 
 import timeutil "github.com/iwind/TeaGo/utils/time"
 
+type TrafficLimitTarget = string
+
+const (
+	TrafficLimitTargetTraffic TrafficLimitTarget = "traffic"
+	TrafficLimitTargetRequest TrafficLimitTarget = "request"
+)
+
 // TrafficLimitStatus 流量限制状态
 type TrafficLimitStatus struct {
-	UntilDay string `yaml:"untilDay" json:"untilDay"` // 有效日期，格式YYYYMMDD
-	PlanId   int64  `yaml:"planId" json:"planId"`     // 套餐ID
-	DateType string `yaml:"dateType" json:"dateType"` // 日期类型 day|month
+	UntilDay   string `yaml:"untilDay" json:"untilDay"`     // 有效日期，格式YYYYMMDD
+	PlanId     int64  `yaml:"planId" json:"planId"`         // 套餐ID
+	DateType   string `yaml:"dateType" json:"dateType"`     // 日期类型 day|month
+	TargetType string `yaml:"targetType" json:"targetType"` // 限制类型：traffic|request|...
 }
 
 func (this *TrafficLimitStatus) IsValid() bool {
