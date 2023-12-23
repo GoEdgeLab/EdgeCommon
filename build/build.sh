0a5d19a87fb67c274274e867c6bec3af61229141
goedgecloud <goedgecloud@gmail.com>
2023-12-23 10:04:37 +0800

    优化编译脚本

--- build/build.sh
+++ build/build.sh
@@ -2,27 +2,27 @@
 
 echo "starting ..."
 
+function assert() {
+	RESULT=$?
+	if [ "${RESULT}" != "0" ]; then
+		exit
+	fi
+}
+
 #rm -f ../pkg/rpc/pb/*.pb.go
 protoc --go_out=../pkg/rpc --proto_path=../pkg/rpc/protos  ../pkg/rpc/protos/*.proto
-RESULT=$?
-if [ "${RESULT}" != "0" ]; then
-	exit
-fi
+assert
 
 protoc --go-grpc_out=../pkg/rpc --go-grpc_opt=require_unimplemented_servers=false --proto_path=../pkg/rpc/protos  ../pkg/rpc/protos/*.proto
-RESULT=$?
-if [ "${RESULT}" != "0" ]; then
-	exit
-fi
+assert
 
 protoc --go_out=../pkg/rpc --proto_path=../pkg/rpc/protos ../pkg/rpc/protos/models/*.proto
 RESULT=$?
-if [ "${RESULT}" != "0" ]; then
-	exit
-fi
+assert
 
 
 # generate rpc.json
 ./proto-json.sh --quiet
+assert
 
 echo "ok"
