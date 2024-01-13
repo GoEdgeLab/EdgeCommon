e74b723ac06fb4e5ba5c32c4e996c93f8a52c136
goedgecloud <goedgecloud@gmail.com>
2024-01-13 16:28:45 +0800

    字符编码设置增加“强制替换”选项

--- pkg/serverconfigs/http_charset_config.go
+++ pkg/serverconfigs/http_charset_config.go
@@ -1,16 +1,17 @@
 package serverconfigs
 
-// 字符集设置
+// HTTPCharsetConfig 字符集设置
 type HTTPCharsetConfig struct {
 	IsPrior bool   `yaml:"isPrior" json:"isPrior"` // 是否覆盖
 	IsOn    bool   `yaml:"isOn" json:"isOn"`       // 是否启用
 	Charset string `yaml:"charset" json:"charset"` // 字符集
 	IsUpper bool   `yaml:"isUpper" json:"isUpper"` // 是否要大写
+	Force   bool   `yaml:"force" json:"force"`     // 是否强制覆盖
 
 	// TODO 支持自定义字符集
 }
 
-// 初始化
+// Init 初始化
 func (this *HTTPCharsetConfig) Init() error {
 	return nil
 }
