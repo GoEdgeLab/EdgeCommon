4be5b6b3e8f871a896213f4196e57abfafb746cf
goedgecloud <goedgecloud@gmail.com>
2024-04-06 20:32:48 +0800

    优化防火墙相关代码

--- pkg/serverconfigs/firewallconfigs/firewall_scope.go
+++ pkg/serverconfigs/firewallconfigs/firewall_scope.go
@@ -5,6 +5,6 @@ package firewallconfigs
 type FirewallScope = string
 
 const (
-	FirewallScopeGlobal  FirewallScope = "global"
-	FirewallScopeService FirewallScope = "service"
+	FirewallScopeGlobal FirewallScope = "global"
+	FirewallScopeServer FirewallScope = "service" // 历史原因，代号为 service 而非 server
 )
